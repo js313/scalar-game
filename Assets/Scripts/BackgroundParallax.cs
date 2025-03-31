fa18c0c9136304b6dd4c744361a8d69ba201a12f
js313 <sharmajeenit2000@gmail.com>
2025-04-01 01:02:12 +0530

    player movement

---
+++ Assets/Scripts/BackgroundParallax.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class BackgroundParallax : MonoBehaviour
+{
+    Camera cam;
+    SpriteRenderer sr;
+
+    [SerializeField] float parallaxSpeed;
+    float xPosition;
+    float len;
+
+
+    void Start()
+    {
+        cam = Camera.main;
+        sr = GetComponent<SpriteRenderer>();
+        xPosition = transform.position.x;
+        len = sr.bounds.size.x;
+    }
+
+    void Update()
+    {
+        float distanceMoved = cam.transform.position.x * (1 - parallaxSpeed);   // Why??
+        float distanceToMove = cam.transform.position.x * parallaxSpeed;
+        transform.position = new Vector3(xPosition + distanceToMove, transform.position.y, transform.position.z);
+        if (distanceMoved > len + xPosition)
+        {
+            xPosition += len;
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,27 @@ public class Player : MonoBehaviour
 
     [Header("Movement Info")]
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float jumpForce;
+    private float jumpForce;
+    [SerializeField] private float defaultJumpForce;
+    [SerializeField] private float doubleJumpForce;
     private bool runBegun = false;
 
     [Header("Collision Info")]
     [SerializeField] private float groundCheckDistance;
     [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private Vector2 wallCheckSize;
     private bool isOnGround = false;
+    private bool canDoubleJump = false;
+    private bool isFacingWall = false;
+
+    [Header("Slide Info")]
+    [SerializeField] private float slideSpeed;
+    [SerializeField] private float slideTimer;
+    [SerializeField] private float slideCooldownTimer;
+    private float slideTimerCounter;
+    private float slideCooldownTimerCounter;
+    private bool isSliding = false;
 
     private void Awake()
     {
@@ -23,21 +37,37 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
-
+        jumpForce = defaultJumpForce;
     }
 
     private void Update()
     {
-        if (runBegun)
-            rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
+        if (runBegun && !isFacingWall)
+            HandleMove();
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.down, groundCheckDistance, whatIsGround);
-        isOnGround = hit;
-        anim.SetBool("isOnGround", isOnGround);
-        anim.SetFloat("xVelocity", rb.linearVelocityX);
-        anim.SetFloat("yVelocity", rb.linearVelocityY);
+        slideTimerCounter -= Time.deltaTime;
+        slideCooldownTimerCounter -= Time.deltaTime;
+
+        RaycastHit2D groundHit = Physics2D.Raycast(transform.position, Vector3.down, groundCheckDistance, whatIsGround);
+        isOnGround = groundHit;
+        if(isOnGround) canDoubleJump = true;
+
+        RaycastHit2D wallHit = Physics2D.BoxCast(wallCheck.position, wallCheckSize, 0, Vector2.zero, 0, whatIsGround);
+        isFacingWall = wallHit;
+
+        AnimatorControllers();
 
         CheckInput();
+    }
+
+    private void AnimatorControllers()
+    {
+        anim.SetBool("isOnGround", isOnGround);
+        anim.SetBool("canDoubleJump", canDoubleJump);
+        anim.SetBool("isSliding", isSliding);
+
+        anim.SetFloat("xVelocity", rb.linearVelocityX);
+        anim.SetFloat("yVelocity", rb.linearVelocityY);
     }
 
     private void CheckInput()
@@ -45,12 +75,57 @@ public class Player : MonoBehaviour
         if (Input.GetButtonDown("Fire2"))
             runBegun = true;
 
-        if (Input.GetButtonDown("Jump") && isOnGround)
+        if (Input.GetButtonDown("Jump"))
+            HandleJump();
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            HandleSlide();
+    }
+
+    private void HandleJump()
+    {
+        if (isOnGround)
+        {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            canDoubleJump = true;
+            jumpForce = doubleJumpForce;
+        }
+        else if (canDoubleJump)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            canDoubleJump = false;
+            jumpForce = defaultJumpForce;
+        }
+    }
+
+    private void HandleMove()
+    {
+        // When under a "roof" the box cast keeps detecting a wall and never calls "HandleMove" so we keep sliding
+        // And as we are resetting the "isSliding" flag in here it gives us the desired output
+        if (isOnGround && isSliding && slideTimerCounter >= 0)
+        {
+            rb.linearVelocity = new Vector2(slideSpeed, rb.linearVelocity.y);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
+            isSliding = false;
+        }
+    }
+
+    private void HandleSlide()
+    {
+        if (rb.linearVelocity.x > 0 && slideCooldownTimerCounter < 0)
+        {
+            isSliding = true;
+            slideTimerCounter = slideTimer;
+            slideCooldownTimerCounter = slideCooldownTimer;
+        }
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawLine(transform.position, transform.position + Vector3.down * groundCheckDistance);
+        Gizmos.DrawWireCube(wallCheck.position, wallCheckSize);
     }
 }
